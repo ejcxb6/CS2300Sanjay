51f28464d8134429f4ed2a61ec7b108cfca01e3e
djdn34 <64755512+djdn34@users.noreply.github.com>
2020-05-04 18:28:07 -0500

    Add files via upload

--- cs2300db.sql
+++ cs2300db.sql
@@ -1,21 +1,20 @@
 use cs2300;
-
-CREATE TABLE `user` (
-  `nameUser` varchar(45) DEFAULT NULL,
-  `idUser` int NOT NULL,
-  PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `student` (
   `idstudent` int NOT NULL,
   `Major` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idstudent`),
-  CONSTRAINT `IDstudent` FOREIGN KEY (`idstudent`) REFERENCES `user` (`idUser`)
+  PRIMARY KEY (`idstudent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `teacher` (
   `idTeacher` int NOT NULL,
   `Department` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idTeacher`),
-  CONSTRAINT `IDTeacehr` FOREIGN KEY (`idTeacher`) REFERENCES `user` (`idUser`)
+  PRIMARY KEY (`idTeacher`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `user` (
+  `nameUser` varchar(45) DEFAULT NULL,
+  `idUser` int NOT NULL,
+  PRIMARY KEY (`idUser`),
+  CONSTRAINT `ID Student` FOREIGN KEY (`idUser`) REFERENCES `student` (`idstudent`),
+  CONSTRAINT `ID Teacher` FOREIGN KEY (`idUser`) REFERENCES `teacher` (`idTeacher`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `course` (
   `courseID` int NOT NULL,
@@ -29,16 +28,6 @@ CREATE TABLE `section` (
   `Class_Size` int DEFAULT NULL,
   `Date_Time` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Section_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `takes` (
-  `section_number` int NOT NULL,
-  `ID` int DEFAULT NULL,
-  `Grade` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`section_number`),
-  KEY `studentID_idx` (`ID`),
-  CONSTRAINT `section number` FOREIGN KEY (`section_number`) REFERENCES `section` (`Section_number`),
-  CONSTRAINT `studentID` FOREIGN KEY (`ID`) REFERENCES `student` (`idstudent`),
-  CONSTRAINT `TeacherID` FOREIGN KEY (`ID`) REFERENCES `teacher` (`idTeacher`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `has` (
   `CourseID` int NOT NULL,
@@ -55,4 +44,13 @@ CREATE TABLE `teaches` (
   CONSTRAINT `Student ID` FOREIGN KEY (`ID`) REFERENCES `student` (`idstudent`),
   CONSTRAINT `Teacher ID` FOREIGN KEY (`ID`) REFERENCES `teacher` (`idTeacher`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+CREATE TABLE `takes` (
+  `section_number` int NOT NULL,
+  `ID` int DEFAULT NULL,
+  `Grade` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`section_number`),
+  KEY `studentID_idx` (`ID`),
+  CONSTRAINT `section number` FOREIGN KEY (`section_number`) REFERENCES `section` (`Section_number`),
+  CONSTRAINT `studentID` FOREIGN KEY (`ID`) REFERENCES `student` (`idstudent`),
+  CONSTRAINT `TeacherID` FOREIGN KEY (`ID`) REFERENCES `teacher` (`idTeacher`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
