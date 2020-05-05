6ef4742c47c12440b5e6a74e41674480f7b7fb40
ejcxb6 <ejcxb6@umsystem.edu>
2020-05-04 20:41:35 -0500

    server connected, login page works.

--- CoronaUniversityDatabase/CoronaUniversityDatabase/.xaml.cs
+++ CoronaUniversityDatabase/CoronaUniversityDatabase/.xaml.cs
@@ -22,40 +22,36 @@ namespace CoronaUniversityDatabase
             string connectionString = "SERVER=localhost;DATABASE=Phase3; UID=root;PASSWORD=h1NN1hAA*26;";
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
-            //MySqlCommand cmd = new MySqlCommand("select from users where idUser=12510114", connection);
-            MySqlCommand cmd = new MySqlCommand("SELECT COUNT(1) FROM user WHERE nameUser = @nameUser AND idUser = @idUser", connection);
+            MySqlCommand cmd = new MySqlCommand("SELECT COUNT(1) FROM teacher,user WHERE idTeacher = @idTeacher AND idTeacher = idUser AND nameUser=@nameUser", connection);
             cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@idTeacher", PWD_TEXT.Text);
             cmd.Parameters.AddWithValue("@nameUser", USER_TEXT.Text);
-            cmd.Parameters.AddWithValue("@idUser", PWD_TEXT.Text);
             int count = Convert.ToInt32(cmd.ExecuteScalar());
             if (count == 1)
             {
                 TeacherScreen teacherWindow = new TeacherScreen();
                 teacherWindow.Show();
+                this.Close();
             }
             else
             {
-                MessageBox.Show("Wrong username or password!");
+                MySqlCommand cmd2 = new MySqlCommand("SELECT COUNT(1) FROM student,user WHERE idstudent = @idstudent AND idUser=idstudent AND nameUser = @nameUser", connection);
+                cmd2.CommandType = CommandType.Text;
+                cmd2.Parameters.AddWithValue("@idstudent", PWD_TEXT.Text);
+                cmd2.Parameters.AddWithValue("@nameUser", USER_TEXT.Text);
+                int count2 = Convert.ToInt32(cmd2.ExecuteScalar());
+                if (count2 == 1)
+                {
+                    StudentScreen studentWindow = new StudentScreen();
+                    studentWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong username or password!");
+                }
             }
             connection.Close();
-            //SqlConnection connection = new SqlConnection("SERVER=localhost;DATABASE=Phase3; UID=root;PASSWORD=h1NN1hAA*26;");
-            ////connection.Open();
-            //string nameUser = USER_TEXT.Text;
-            //string idUser = PWD_TEXT.Text;
-            //SqlCommand cmd = new SqlCommand("SELECT nameUser, idUser FROM User WHERE nameUser = '" + USER_TEXT.Text + "'and idUser = '" + PWD_TEXT.Text + "'", connection);
-            //SqlDataAdapter da = new SqlDataAdapter(cmd);
-            //DataTable dt = new DataTable();
-            //da.Fill(dt);
-            //if (dt.Rows.Count > 0)
-            //{
-            //    TeacherScreen teacherWindow = new TeacherScreen();
-            //    teacherWindow.Show();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Invalid Login please check username and password");
-            //}
-            ////connection.Close();
         }
 
     }
