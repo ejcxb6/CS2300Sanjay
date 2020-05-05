83dd6f351eb1502fd08e8803b5ff723a0a6d9d54
ejcxb6 <ejcxb6@umsystem.edu>
2020-05-04 20:13:13 -0500

    server works on login!

--- CoronaUniversityDatabase/CoronaUniversityDatabase/.xaml.cs
+++ CoronaUniversityDatabase/CoronaUniversityDatabase/.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Data.Sql;
-using System.Data.SqlClient;
+﻿using System.Windows;
 using System.Data;
+using System;
+using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace CoronaUniversityDatabase
 {
@@ -23,33 +12,50 @@ namespace CoronaUniversityDatabase
     /// </summary>
     public partial class MainWindow : Window
     {
-        SqlConnection sqlCon = new SqlConnection();
         public MainWindow()
         {
-            SqlConnection sqlCon = new SqlConnection(@"Data Source=Local instance MySql80; Initial Catalog=Phase3; Integrated Security=True;");
             InitializeComponent();
         }
 
         private void LoginBtn_Click(object sender, RoutedEventArgs e)
         {
-            SqlConnection sqlCon = new SqlConnection(@"Data Source=Local instance MySql80; Initial Catalog=Phase3; Integrated Security=True;");
-            sqlCon.Open();
-            string nameUser = USER_TEXT.Text;
-            string idUser = PWD_TEXT.Text;
-            SqlCommand cmd = new SqlCommand("SELECT nameUser, idUser FROM User WHERE nameUser = '" + USER_TEXT.Text + "'and idUser = '" + PWD_TEXT.Text + "'", sqlCon);
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            if (dt.Rows.Count > 0)
+            string connectionString = "SERVER=localhost;DATABASE=Phase3; UID=root;PASSWORD=h1NN1hAA*26;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            //MySqlCommand cmd = new MySqlCommand("select from users where idUser=12510114", connection);
+            MySqlCommand cmd = new MySqlCommand("SELECT COUNT(1) FROM user WHERE nameUser = @nameUser AND idUser = @idUser", connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@nameUser", USER_TEXT.Text);
+            cmd.Parameters.AddWithValue("@idUser", PWD_TEXT.Text);
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            if (count == 1)
             {
                 TeacherScreen teacherWindow = new TeacherScreen();
                 teacherWindow.Show();
             }
             else
             {
-                MessageBox.Show("Invalid Login please check username and password");
+                MessageBox.Show("Wrong username or password!");
             }
-            sqlCon.Close();
+            connection.Close();
+            //SqlConnection connection = new SqlConnection("SERVER=localhost;DATABASE=Phase3; UID=root;PASSWORD=h1NN1hAA*26;");
+            ////connection.Open();
+            //string nameUser = USER_TEXT.Text;
+            //string idUser = PWD_TEXT.Text;
+            //SqlCommand cmd = new SqlCommand("SELECT nameUser, idUser FROM User WHERE nameUser = '" + USER_TEXT.Text + "'and idUser = '" + PWD_TEXT.Text + "'", connection);
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //if (dt.Rows.Count > 0)
+            //{
+            //    TeacherScreen teacherWindow = new TeacherScreen();
+            //    teacherWindow.Show();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Invalid Login please check username and password");
+            //}
+            ////connection.Close();
         }
 
     }
