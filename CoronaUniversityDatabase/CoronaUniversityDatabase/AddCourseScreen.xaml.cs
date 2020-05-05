76ce8c44d178d3a372f1cabbcbeb404601ae458d
ejcxb6 <ejcxb6@umsystem.edu>
2020-05-05 13:16:22 -0500

    successful sql command for add course screen

--- CoronaUniversityDatabase/CoronaUniversityDatabase/AddCourseScreen.xaml.cs
+++ CoronaUniversityDatabase/CoronaUniversityDatabase/AddCourseScreen.xaml.cs
@@ -15,15 +15,12 @@ namespace CoronaUniversityDatabase
             string connectionString = "SERVER=localhost;DATABASE=Phase3; UID=root;PASSWORD=h1NN1hAA*26;";
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
-            MySqlCommand cmd = new MySqlCommand("select course.courseID, course.Department, course.creditHours, " +
-                "course.Name FROM student,takes,section,has,course where student.idstudent=@studentID AND " +
-                "student.idstudent = takes.ID AND takes.SectionNumber = section.SectionNumber AND " +
-                "section.SectionNumber = has.SectionNumber AND course.courseID = has.CourseID", connection);
-            //MySqlCommand cmd = new MySqlCommand("SELECT course.courseID, course.Department, course.creditHours, " +
-            //    "course.Name FROM student,takes,section,has,course WHERE student.idstudent = takes.ID AND " +
-            //    "takes.SectionNumber = section.SectionNumber AND takes.SectionNumber <> has.SectionNumber" +
-            //    " AND section.SectionNumber = has.SectionNumber AND course.courseID = has.CourseID" +
-            //    " AND student.idstudent = @studentID", connection);
+            MySqlCommand cmd = new MySqlCommand("select distinct course.courseID, course.Department, course.creditHours," +
+                " course.Name FROM student, takes, section, has, course where course.courseID" +
+                " not in (select course.courseID FROM student, takes, section, has, course" +
+                " where student.idstudent = 12510114 AND" +
+                " student.idstudent = takes.ID AND takes.SectionNumber = section.SectionNumber AND" +
+                " section.SectionNumber = has.SectionNumber AND course.courseID = has.CourseID)", connection);
             cmd.Parameters.AddWithValue("@studentID", user.id);
             DataTable dt = new DataTable();
             dt.Load(cmd.ExecuteReader());
